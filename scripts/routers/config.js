9a1f971a2ca221d82ca4b2ec101b72bd5afc0991
Wentao LI <liwentao90@yahoo.com>
2019-06-19 23:19:03 -0400

    - initially support PUBLIC_URL from process.env 9#

--- scripts/routers/config.js
+++ scripts/routers/config.js
@@ -38,7 +38,8 @@ router.get('/app.json',(req,res)=>{
 
     if (
         process.env.PUBLIC_URL &&
-        process.env.PUBLIC_URL.indexOf('/') === 0
+        process.env.PUBLIC_URL.indexOf('/') === 0 &&
+        process.env.PUBLIC_URL.length > 0
     ){
         baseUrl+=process.env.PUBLIC_URL;
     }
